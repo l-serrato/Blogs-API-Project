8ef9fada2c2e7ff4afb8bb4c1cf451c2fdb1e3ba
Luis <l-serrato@live.com>
2023-05-11 17:22:06 -0300

    req04 test

--- src/controllers/createUser.js
+++ src/controllers/createUser.js
@@ -7,12 +7,12 @@ const secret = process.env.JWT_SECRET;
 const validateBody = (body) =>
   
   Joi.object({
+    password: Joi.string().min(6).required().messages({
+      'string.min': '"password" length must be at least 6 characters long',
+    }),
     displayName: Joi.string().min(8).required()
     .messages({
       'string.min': '"displayName" length must be at least 8 characters long',
-    }),
-    password: Joi.string().min(6).required().messages({
-      'string.min': '"password" length must be at least 6 characters long',
     }),
     email: Joi.string().min(1).email().required()
     .messages({
